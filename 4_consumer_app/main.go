ab1491d617c8b3435ab0544b1e9830addef94494
JY8752 <j.y.8752@gmail.com>
2023-07-24 17:26:19 +0900

    consumer_app

--- 4_consumer_app/main.go
+++ 4_consumer_app/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/segmentio/kafka-go"
+)
 
 func main() {
 	fmt.Println("Consumer Started.")
+
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:  []string{"localhost:9092", "localhost:9093", "localhost:9094"},
+		GroupID:  "consumer-group-id",
+		Topic:    "topic-A",
+		MaxBytes: 10e6, // 10MB
+	})
 }
